c98928886da746261e94b5c450e8db18fe0a718f
Kainato <caiocaladaraujo@gmail.com>
2026-08-25 13:59:58 -0300

    Create 1116_dividindo_x_por_y.dart
    Dividindo X por Y

---
+++ lib/beecrowd/nivel_1/1116_dividindo_x_por_y.dart
@@ -0,0 +1,31 @@
+/* Dividindo X por Y
+
+Escreva um algoritmo que leia 2 números e imprima o resultado da divisão do primeiro pelo segundo. Caso não for possível mostre a mensagem “divisao impossivel” para os valores em questão.
+
+Entrada
+A entrada contém um número inteiro N. Este N será a quantidade de pares de valores inteiros (X e Y) que serão lidos em seguida.
+
+Saída
+Para cada caso mostre o resultado da divisão com um dígito após o ponto decimal, ou “divisao impossivel” caso não seja possível efetuar o cálculo.
+
+Obs.: Cuide que a divisão entre dois inteiros em algumas linguagens como o C e C++ gera outro inteiro. Utilize cast :)
+*/
+
+import 'dart:io';
+
+void main() {
+  int n = int.parse(stdin.readLineSync()!);
+
+  for (int i = 0; i < n; i++) {
+    List<int> valores = stdin.readLineSync()!.split(' ').map(int.parse).toList();
+
+    int x = valores[0];
+    int y = valores[1];
+
+    if (y == 0) {
+      print('divisao impossivel');
+    } else {
+      print((x / y).toStringAsFixed(1));
+    }
+  }
+}
